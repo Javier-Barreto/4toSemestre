9ff0b5c6fde03c27953329362a86721a6166cd80
Javier-Barreto <60673812+Javier-Barreto@users.noreply.github.com>
2021-03-27 21:24:58 -0600

    Add files via upload

---
+++ API_SD/APP/models/producto.js
@@ -0,0 +1,10 @@
+class producto{
+    constructor(nombre, numsucursal, precio, cantidad){
+        this.id=null;
+        this.sucursal=numsucursal;
+        this.nombre=nombre;
+        this.precio=precio
+        this.cantidad=cantidad;
+    }
+}
+module.exports=producto;
